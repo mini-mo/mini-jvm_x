6809c690b4daabf6b21db07056e9f4a096a66dfb
guxingke <admin@guxingke.com>
2022-01-12 19:08:24 +0800

    :sparkles: .

---
+++ c/README.md
@@ -0,0 +1,5 @@
+# Mini-jvm_c
+
+mini-jvm 重写版，C 语言实现.
+
+
---
+++ c/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char u1;
+typedef unsigned short u2;
+typedef unsigned int u4;
+typedef unsigned long long u8;
+
+
+#define READ_U1(v, p, l)  v = *(p)++
+#define READ_U2(v, p, l)  v = ((p)[0]<<8)|(p)[1]; (p)+=2
+#define READ_U4(v, p, l)  v = ((p)[0]<<24)|((p)[1]<<16)|((p)[2]<<8)|(p)[3]; (p)+=4
+#define READ_U8(v, p, l)  v = ((u8)(p)[0]<<56)|((u8)(p)[1]<<48)|((u8)(p)[2]<<40) \
+                              |((u8)(p)[3]<<32)|((u8)(p)[4]<<24)|((u8)(p)[5]<<16) \
+|((u8)(p)[6]<<8)|(u8)(p)[7]; (p)+=8
+#define SKIP_U2(v, p, l)  (p)+=2
+
+#define READ_INDEX(v, p, l)               READ_U2(v,p,l)
+#define READ_TYPE_INDEX(v, cp, t, p, l)     READ_U2(v,p,l)
+
+int main(int argc, char **argv) {
+
+  char *exe = argv[0];
+  char *cls = argv[1];
+  char *p1 = argv[2];
+  char *p2 = argv[3];
+
+  // printf("%s %s %s %s\n", exe, cls, p1, p2);
+
+  FILE *fd = fopen("../misc/Hello.class", "r");
+  if ( fd == NULL ) {
+    exit(-1);
+  }
+
+  fseek(fd, 0L, SEEK_END);
+  int file_len = ftell(fd);
+  fseek(fd, 0L, SEEK_SET);
+
+  // printf("file len: %d\n", file_len);
+
+  char *data = malloc(file_len);
+  int read_len = fread(data, sizeof(char), file_len, fd);
+  fclose(fd);
+  // printf("read len: %d\n", file_len);
+
+  if ( read_len != file_len ) {
+    exit(-2);
+  }
+
+  // read byte
+  u1 *ptr = (u1 *)data;
+  int magic;
+  READ_U4(magic, ptr, read_len);
+  if (magic != 0xcafebabe) {
+    printf("bad magic");
+    exit(-3);
+  }
+  printf("0x%X\n", magic);
+  (ptr)+=2; // mv
+  (ptr)+=2; // mv
+
+  int cp_count;
+  READ_U2(cp_count, ptr, read_len);
+  printf("cp cnt: %d\n", cp_count);
+
+}
